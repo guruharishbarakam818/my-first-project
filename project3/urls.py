bf00273326a991a3f0414133a2574a417f8bb49d
guruharishbarakam818 <guruharishbarakam818@gmail.com>
2022-11-25 22:29:25 +0530

    first commit

--- project3/urls.py
+++ project3/urls.py
@@ -20,5 +20,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('harish/',harish,name='harish'),
-    path('guru/',sunil,name='guru'),
+    path('sunil/',sunil,name='sunil'),
 ]
